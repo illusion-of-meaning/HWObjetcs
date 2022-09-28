8c56b4035104190f6700d9089bcec73967c6889f
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-09-28 20:28:25 +0900

    Complite

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author authorBulgakov = new Author("Булгаков", "Михаил");
+        Author authorPushkin = new Author("Пушкин", "Александр");
+        Book bookFirst = new Book("Я убил", 926, authorBulgakov);
+        Book bookSecond = new Book("Сказка о царе Салтане", 1831, authorPushkin);
+        System.out.println(bookFirst);
+        bookFirst.setAge(1926);
+        System.out.println(bookFirst);
     }
 }
